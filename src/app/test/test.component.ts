e0d359f2ff86bdd3697f51391e830c089efeaa08
manne0081@gmail.com <manne0081@gmail.com>
2023-03-05 23:23:52 +0100

    testEdited

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { last } from 'rxjs';
 
 @Component({
   selector: 'app-test',
@@ -6,10 +7,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./test.component.scss']
 })
 export class TestComponent implements OnInit {
+	number: string = "";
+	firstName: string = "";
+	lastName: string = "";
+	street: string = "";
+	houseNumber: string = "";
+	postCode: string = "";
+	city: string = "";
+	objectName: string = "";
 
-  constructor() { }
+	constructor() { }
 
-  ngOnInit(): void {
-  }
+	ngOnInit(): void {
+		this.number = "KND-00105015-01";
+		this.firstName = "Max";
+		this.lastName = "Mustermann";
+		this.street = "Musterstraße";
+		this.houseNumber = "128 a-b";
+		this.postCode = "12345"
+		this.city = "Musterhausen";
+		this.objectName = this.number + " - " + this.firstName + " " + this.lastName;
+	}
 
 }
