f8811831b9573847ad7d8cfd11806a80c2de7e15
ManuelSchmidt <manne0081@gmail.com>
2024-08-23 15:33:35 +0200

    added some components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+// PUBLIC-COMPONENTS
+// *****************
+import { HomeComponent } from './modules/public/home/home.component';
+import { AuthComponent } from './modules/public/auth/auth.component';
+
+// PRIVATE-COMPONENTS
+// ******************
+import { DashboardComponent } from './modules/private/dashboard/dashboard.component';
+import { PrivateComponent } from './modules/private/private.component';
+
+// ROUTES
+// ******
+export const routes: Routes = [
+    { path: '', redirectTo: 'home', pathMatch: 'full' },
+
+    { path: 'home', component: HomeComponent },
+    { path: 'auth', component: AuthComponent },
+
+    { path: 'private', component: PrivateComponent,
+        children: [
+            { path: 'dashboard', component: DashboardComponent },
+        ]
+    }
+];
